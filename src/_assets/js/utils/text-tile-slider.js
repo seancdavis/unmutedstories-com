153ff62f06171c4ae280465147b6bb003d03724b
Sean C Davis <scdavis41@gmail.com>
2020-10-27 05:47:53 -0400

    Swap out glider for slider

--- src/_assets/js/utils/text-tile-slider.js
+++ src/_assets/js/utils/text-tile-slider.js
@@ -1,15 +1,22 @@
-import Glide from "@glidejs/glide"
+import Swiper, { Navigation } from "swiper"
+
+// configure Swiper to use modules
+Swiper.use([Navigation])
 
 class TextTileSlider {
   constructor() {}
 
-  static init(selector = ".glide") {
-    new Glide(selector, {
-      gap: 24,
-      peek: 100,
-      perView: 3,
-      rewind: false
-    }).mount()
+  static init(selector = ".swiper-container") {
+    new Swiper(selector, {
+      navigation: {
+        nextEl: ".swiper-next-button",
+        prevEl: ".swiper-prev-button"
+      },
+      slidesOffsetAfter: 100,
+      slidesOffsetBefore: 100,
+      slidesPerView: 3,
+      spaceBetween: 32
+    })
   }
 }
 
