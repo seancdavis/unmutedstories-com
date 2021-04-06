a066951320ee0f87f17ae675024dc8dda38cc9e9
Sean C Davis <scdavis41@gmail.com>
2021-04-06 11:00:57 -0400

    Remove function

--- netlify/functions/slack-invite.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async function (event, context, callback) {
-  console.log(JSON.stringify(event), JSON.stringify(context));
-
-  callback(null, { statusCode: 200, body: "WIP ..." });
-};
