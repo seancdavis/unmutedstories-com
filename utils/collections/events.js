832b5287f52d99cb25afc3e96f654284d3b67639
Sean C Davis <scdavis41@gmail.com>
2021-04-29 06:22:45 -0400

    Hide events from home page when there are no upcoming events

--- utils/collections/events.js
+++ utils/collections/events.js
@@ -1,4 +1,14 @@
 /**
+ * Collects and returns all events in chronological order.
+ */
+const getAllEvents = (collectionApi) => {
+  return collectionApi
+    .getAll()
+    .filter((item) => item.data.layout === "event")
+    .sort((a, b) => b.date - a.date);
+};
+
+/**
  * Provides a method for adding NJK components.
  *
  * @param {object} eleventyConfig Eleventy's configuration object
@@ -8,11 +18,15 @@ exports.default = (eleventyConfig) => {
    * Creates the "events" collection from the "event" layout.
    */
   eleventyConfig.addCollection("events", (collectionApi) => {
-    const events = collectionApi
-      .getAll()
-      .filter((item) => item.data.layout === "event")
-      .sort((a, b) => b.date - a.date);
-
-    return events;
+    return getAllEvents(collectionApi);
+  });
+  /**
+   * Creates the "upcoming_events" collection by pulling all events and
+   * filtering out those from the past.
+   */
+  eleventyConfig.addCollection("upcoming_events", (collectionApi) => {
+    return getAllEvents(collectionApi)
+      .filter((event) => event.date >= Date.now())
+      .reverse();
   });
 };
